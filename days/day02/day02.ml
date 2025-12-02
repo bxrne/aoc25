27fe474e8e573742d59772786867ea4491070618
Adam Byrne <adamrbyrne@gmail.com>
2025-12-02 20:54:20 +0000

    refactor(day02): Refactored part 1 to be more reusable

--- days/day02/day02.ml
+++ days/day02/day02.ml
@@ -1,69 +1,68 @@
 module Solution = struct
-  (* Problem domain type *)
-  type range_value = {
-    start: int;
-    end_: int;
+  (** Represents a range of integers [start, end_pos] inclusive *)
+  type range = {
+    start : int;
+    end_pos : int;
   }
 
-  (* Accept string input and give list of lines *)
-  let parse_input (input: string) : string list =
+  (** Parse non-empty lines from input string *)
+  let parse_input (input : string) : string list =
     String.split_on_char '\n' input
-    |> List.filter (fun line -> String.length line > 0)
+    |> List.filter (fun line -> String.trim line <> "")
 
-  (* Parse a single range from string to range_value *)
-  let parse_range (input: string) : range_value =
-    let bounds = String.split_on_char '-' input in
-    match bounds with
-    | [ start_str; end_str ] ->
-        {
-          start = int_of_string start_str;
-          end_ = int_of_string end_str;
-        }
-    | _ -> failwith "Invalid range format"
+  (** Parse a single range from "start-end" format *)
+  let parse_range (s : string) : range =
+    match String.split_on_char '-' s with
+    | [start_str; end_str] ->
+        { start = int_of_string start_str;
+          end_pos = int_of_string end_str }
+    | _ -> 
+        failwith (Printf.sprintf "Invalid range format: '%s'" s)
 
-  (* Parse multiple ranges from list of strings *)
-  let parse_ranges (input : string list) : range_value list =
+  (** Parse comma-separated ranges from multiple lines *)
+  let parse_ranges (lines : string list) : range list =
+    lines
+    |> List.map (String.split_on_char ',')
+    |> List.flatten
+    |> List.map parse_range
+
+  (** Check if a number has a leading zero (when > 9) *)
+  let has_leading_zero (n : int) : bool =
+    let s = string_of_int n in
+    String.length s > 1 && s.[0] = '0'
+
+  (** Check if first half of string equals second half *)
+  let has_repeating_halves (s : string) : bool =
+    let len = String.length s in
+    if len mod 2 <> 0 then false
+    else
+      let half = len / 2 in
+      String.sub s 0 half = String.sub s half half
+
+  (** A value is invalid if it has leading zeros or repeating halves *)
+  let is_invalid_value (n : int) : bool =
+    let s = string_of_int n in
+    has_leading_zero n || has_repeating_halves s
+
+  (** Generate all integers in a range [start, end_pos] *)
+  let range_to_list (r : range) : int list =
+    List.init (r.end_pos - r.start + 1) (fun i -> r.start + i)
+
+  (** Collect all invalid values across multiple ranges *)
+  let collect_invalid_values (ranges : range list) : int list =
+    ranges
+    |> List.map range_to_list
+    |> List.flatten
+    |> List.filter is_invalid_value
+
+  let part1 (input : string) : string =
     input
-    |> List.map (fun line ->
-           line
-           |> String.split_on_char ','
-           |> List.map parse_range)
-    |> List.concat
+    |> parse_input
+    |> parse_ranges
+    |> collect_invalid_values
+    |> List.fold_left (+) 0
+    |> string_of_int
 
-  let invalid_range_val(valu: int) : bool =
-    match string_of_int valu with
-    (* Cannot have a leading zero *)
-    | s when String.length s > 1 && String.get s 0 = '0' -> true
-    (* First half of string cannot equal the second half *)
-    | s when String.length s mod 2 = 0 ->
-        let half_len = String.length s / 2 in
-        let first_half = String.sub s 0 half_len in
-        let second_half = String.sub s half_len half_len in
-        first_half = second_half
-    | _ -> false
-
-
-  let part1 (input: string) : string =
-    let vals = parse_input input in 
-    let ranges = parse_ranges vals in 
-    let rec collect_invalids (ranges: range_value list) : int list =
-      match ranges with
-      | [] -> []
-      | range :: rest ->
-          let invalids_in_range =
-            List.filter invalid_range_val
-              (List.init (range.end_ - range.start + 1)
-                 (fun i -> range.start + i))
-          in
-          invalids_in_range @ collect_invalids rest
-    in
-    let invalid_sum =
-      collect_invalids ranges
-      |> List.fold_left (fun acc x -> acc + x) 0
-    in 
-    string_of_int invalid_sum 
-
-
-  let part2 (input: string) : string =
+  let part2 (input : string) : string =
     input
 end
